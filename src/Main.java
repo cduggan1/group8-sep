162451a2f3324e8559e637c815e1334b728f08f2
Creagh Duggan <creagh.duggan@corrata.com>
2023-02-07 14:47:32 +0000

    Boilerplate for REST API

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import static spark.Spark.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        get("/hello", (req, res)->"Hello, world");
+
+        get("/hello/:name", (req,res)->{
+            return "Hello, "+ req.params(":name");
+        });
+    }
+}
