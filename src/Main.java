2f7393979cce1416a2c0c57cfc56455c1028463b
Creagh Duggan <creagh.duggan@corrata.com>
2023-02-07 17:33:06 +0000

    API functionality for searching by brand and site name. API functionality for querying if site has ensuite, or studios, or both.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -10,20 +11,51 @@ public class Main {
     public static void main(String[] args) {
 
         List<Map<?, ?>> accoms = buildObject("src/info.csv");
+
         if(accoms==null){
             System.out.printf("Error Parsing CSV");
             System.exit(1);
         }
-        System.out.println(accoms.get(1).toString());
+
             get("/id/:index", (req,res)->{
                 int index = Integer.parseInt(req.params(":index"));
                 System.out.print("Index requested: " + index);
                 return accoms.get(index).toString();
             });
 
+
+
             get("/all", (req,res)->{
                 System.out.print("Requested All");
                 return accoms.toString();
+            });
+
+            get("/name/:name", (req,res)->{
+                return getSiteInfo(accoms, req.params(":name"));
+            });
+
+            get("/brand/:brand", (req,res)->{
+                return getCompanyInfo(accoms, req.params(":brand"));
+            });
+
+            get("/id/:index/:hasEnsuite", (req,res)->{
+                int index = Integer.parseInt(req.params(":index"));
+                System.out.print("Studio Check for index: " + index);
+                if (hasStudios(accoms, index)){
+                    return "Yes";
+                }else {
+                    return "No";
+                }
+            });
+
+            get("/id/:index/:hasEnsuite", (req,res)->{
+                int index = Integer.parseInt(req.params(":index"));
+                System.out.print("Ensuite Check for index: " + index);
+                if (hasEnsuites(accoms, index)){
+                    return "Yes";
+                }else {
+                    return "No";
+                }
             });
 
 
@@ -39,12 +71,40 @@ public class Main {
             CsvSchema csv = CsvSchema.emptySchema().withHeader();
             CsvMapper csvMapper = new CsvMapper();
             MappingIterator<Map<?, ?>> mappingIterator = csvMapper.reader().forType(Map.class).with(csv).readValues(input);
-            List<Map<?, ?>> list = mappingIterator.readAll();
-            return list;
+            return mappingIterator.readAll();
         } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
+    }
+
+    public static boolean hasEnsuites(List<Map<?, ?>> list, int id){
+        return(list.get(id).toString().contains("Has Ensuite=y"));
+    }
+    public static boolean hasStudios(List<Map<?, ?>> list, int id){
+        return(list.get(id).toString().contains("Has Studio=y"));
+    }
+    public static String getSiteInfo(List<Map<?, ?>> list, String site){
+        for(int i=0;i<list.size();i++){
+            if(list.get(i).toString().contains("Site="+site)){
+                return list.get(i).toString();
+            }
+        }
+        return "None";
+    }
+
+    public static String getCompanyInfo(List<Map<?, ?>> list, String company){
+        ArrayList<String> listOfCompany = new ArrayList<String>();
+
+        for(int i=0;i<list.size();i++) {
+            if (list.get(i).toString().contains("Brand=" + company)) {
+                listOfCompany.add(list.get(i).toString());
+            }
+        }
+        if(listOfCompany.size()==0)
+            return "None";
+
+        return listOfCompany.toString();
     }
 
 }
