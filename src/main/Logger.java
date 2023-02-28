b2fd2972847beaf76dc9d4da31ba0b5fd3eafb20
Creagh Duggan <creagh.duggan@corrata.com>
2023-02-28 13:24:38 +0000

    Further Logging functionality implemented.

--- src/main/Logger.java
+++ src/main/Logger.java
@@ -1,13 +1,25 @@
 package main;
 
+import java.io.*;
 import java.util.ArrayList;
 
 public class Logger {
 
     public static ArrayList<String> logFile = new ArrayList<>();
+    public static String epoch = String.valueOf(System.currentTimeMillis());
 
 
     public static void addLog(String tag, String msg) {
+        File file = new File("src/logs/","new_log_"+epoch+".txt");
+
+        try(FileWriter fw = new FileWriter(file, true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter out = new PrintWriter(bw))
+        {
+            out.println(tag+":"+msg);
+        } catch (IOException e) {
+            //exception handling left as an exercise for the reader
+        }
         logFile.add(tag + "," + msg + ".\n");
     }
     public static void clearLog(){
