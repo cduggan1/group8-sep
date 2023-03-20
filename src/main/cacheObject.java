e92cc4de4dd47a840e270df230cf19ee0fe69726
Creagh Duggan <creagh.duggan@corrata.com>
2023-03-20 21:14:18 +0000

    Cacheing object boilerplate.

---
+++ src/main/cacheObject.java
@@ -0,0 +1,23 @@
+package main;
+
+import java.util.HashMap;
+
+public class cacheObject {
+    private static HashMap<String, String> cache = new HashMap<String, String>();
+
+    public static boolean putRequest(String request, String response){
+        try {
+            cache.put(request, response);
+            return true;
+        }catch(Exception e) {
+            return false;
+        }
+    }
+
+    public static String getRequest(String request){
+        return cache.getOrDefault(request, "");
+    }
+
+
+
+}
