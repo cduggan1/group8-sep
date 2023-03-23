f023d7037456d32e9d1e459b8b65d96b28a3242b
Liam Zone <zonel@tcd.ie>
2023-03-23 11:31:41 +0000

    Fixed Amenities Testing w/ imported class

--- src/test/MainTest.java
+++ src/test/MainTest.java
@@ -2,6 +2,7 @@ package test;
 
 import main.Main;
 import main.csvData;
+import main.synonymMapBuilder;
 import org.junit.jupiter.api.BeforeEach;
 
 import java.io.IOException;
@@ -18,23 +19,25 @@ class MainTest {
     @BeforeEach
     void main() throws IOException {
         csvData.init();
+        synonymMapBuilder.init();
     }
 
     @org.junit.jupiter.api.Test
     void filterAccoms() throws IOException {
+        // Test
         FILTERS = new HashMap<>();
         FILTERS.put("Brand", "Yugo");
         FILTERS.put("Site", "Highfield Park");
-        FILTERS.put("Amenities", "disable");
         List<Map<?,?>> accoms = csvData.buildObject("src/main/info.csv");
         String filterMap = Main.filterAccoms(accoms, FILTERS).toString();
         assert(filterMap.contains("Highfield Park"));
-
+        assert(!filterMap.contains("Dominick Place"));
 
         FILTERS.clear();
         FILTERS.put("Amenities", "disable");
         filterMap = Main.filterAccoms(accoms, FILTERS).toString();
         assert(filterMap.contains("Dominick Place") && filterMap.contains("Beckett House"));
+        System.out.println(FILTERS.keySet().toString());
         assert(FILTERS.containsKey("Disability_Access"));
     }
 
