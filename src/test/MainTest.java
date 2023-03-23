bd13dab5cb39fcd5fb8c3630dc2a3b12765a535c
Liam Zone <zonel@tcd.ie>
2023-03-23 11:25:14 +0000

    Added FullQuery Amenities Testing

--- src/test/MainTest.java
+++ src/test/MainTest.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Filter;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -24,9 +25,17 @@ class MainTest {
         FILTERS = new HashMap<>();
         FILTERS.put("Brand", "Yugo");
         FILTERS.put("Site", "Highfield Park");
+        FILTERS.put("Amenities", "disable");
         List<Map<?,?>> accoms = csvData.buildObject("src/main/info.csv");
         String filterMap = Main.filterAccoms(accoms, FILTERS).toString();
         assert(filterMap.contains("Highfield Park"));
+
+
+        FILTERS.clear();
+        FILTERS.put("Amenities", "disable");
+        filterMap = Main.filterAccoms(accoms, FILTERS).toString();
+        assert(filterMap.contains("Dominick Place") && filterMap.contains("Beckett House"));
+        assert(FILTERS.containsKey("Disability_Access"));
     }
 
 
